52afc4e6e76c9aa4a33941c421b4cfeecd9ce13e
Dragos20Tech <dragospetrescu5@gmail.com>
2023-11-22 14:39:37 +0200

    Refactored the code, new roll_dice method added and all methods went non-public

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ class Player:
     def __init__(self, die, is_computer=False):
         self._die = die
         self._is_computer = is_computer
-        self._counter = 10
+        self._counter = 5
 
     # READ-ONLY
     @property
@@ -57,73 +57,73 @@ class DiceGame:
         print("Welcome to Roll the Dice!")
         print("===========================")
         while True:
-            self.play_round()
-            game_over = self.check_game_over()
+            self._play_round()
+            game_over = self._check_game_over()
             if game_over:
                 break
-    def play_round(self):
+
+    def _play_round(self):
         # Welcome the user
-        self.print_round_welcome()
+        self._print_round_welcome()
 
         # Roll the dice
-        player_value = self._player.roll_die()
-        computer_value = self._computer.roll_die()
+        player_value = self._roll_dice(self._player)
+        computer_value = self._roll_dice(self._computer)
 
         # Show the values
-        self.show_dice(player_value, computer_value)
+        self._show_dice(player_value, computer_value)
 
         # Determine winner & loser
         if player_value > computer_value:
             print("You won the round!")
-            self.update_counters(winner=self._player, loser=self._computer)
+            self._update_counters(winner=self._player, loser=self._computer)
         elif computer_value > player_value:
             print("Mission failed... We'll get'em next time!")
-            self.update_counters(winner=self._computer, loser=self._player)
+            self._update_counters(winner=self._computer, loser=self._player)
         else:
             print("It's a tie! ")
 
         # Show counters
-        self.show_counters()
+        self._show_counters()
 
-    def print_round_welcome(self):
+    def _print_round_welcome(self):
         print("\n------- New Round -------")
         input("Press any key to roll the dice!")
 
-    def show_dice(self, player_value, computer_value):
+    def _show_dice(self, player_value, computer_value):
         print(f"\nYour die : {player_value}")
         print(f"Computer die : {computer_value}")
 
-    def update_counters(self, winner, loser):
+    def _update_counters(self, winner, loser):
         winner.decrement_counter()
         loser.increment_counter()
 
-    def show_counters(self):
+    def _show_counters(self):
         print(f"Your counter : {self._player.counter}")
         print(f"Computer counter : {self._computer.counter}\n")
 
-    def check_game_over(self):
+    def _check_game_over(self):
         if self._player.counter == 0:
-            self.show_game_over(self._player)
+            self._show_game_over(self._player)
             return True
         elif self._computer.counter == 0:
-            self.show_game_over(self._computer)
+            self._show_game_over(self._computer)
             return True
         else:
             return False
 
-    def show_game_over(self, winner):
+    def _show_game_over(self, winner):
+        print("\n====================")
+        print("G A M E   O V E R ")
+        print("====================")
         if winner.is_computer:
-            print("\n====================")
-            print("G A M E   O V E R ")
-            print("====================")
             print("Computer won the game. See you next time!")
-            print("===========================================")
         else:
-            print("\n====================")
-            print("G A M E   O V E R ")
-            print("====================")
             print("You won the game! Well done!")
-            print("===========================================")
+        print("===========================================")
+
+    def _roll_dice(self, roller):
+        return roller.roll_die()
 
 
 player_die = Die()
